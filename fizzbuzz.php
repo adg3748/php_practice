9170c262067d8135c38ff3c522ba17731a0e49ca
adg3748 <revivedtomorrow@gmail.com>
2018-04-04 15:09:43 +0900

    fizzbuzz作ってみた

---
+++ fizzbuzz.php
@@ -0,0 +1,14 @@
+<?php
+
+for($i=1;$i<=100;$i++){
+  if($i%3==0 and $i%5==0){
+    print 'fizzbuzz';
+  } else if($i%3==0){
+    print 'fizz';
+  } else if($i%5==0){
+    print 'buzz';
+  } else {
+    print $i;
+  }
+  print "\n"; # ''にすると\nが効かなくなる
+}
